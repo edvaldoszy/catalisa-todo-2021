87fafe6ae15e969834c830a8e8f267980df240a3
Edvaldo Szymonek <edvaldoszy@gmail.com>
2021-10-22 15:50:59 -0300

    Cria função de validação de usuários

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const Joi = require('joi');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+function validacao(request, response, next) {
+  const schema = Joi.object({
+    nome: Joi.string().min(1).max(200).required(),
+    email: Joi.string().min(1).max(200).required(),
+    senha: Joi.string().min(6).max(80).required(),
+  });
+  const resultado = schema.validate(request.body);
+  if (resultado.error) {
+    response.status(400).json(resultado.error);
+  } else {
+    next();
+  }
+}
+
+router.post('/', validacao, function(req, res, next) {
   res.send('respond with a resource');
 });
 
